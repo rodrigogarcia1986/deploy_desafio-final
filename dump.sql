289764bf025f797e1542d4416559c706ff851fa3
Patrick Rigaud <104288701+PatrickRigaud@users.noreply.github.com>
2023-10-08 08:26:39 -0300

    fix (dump) adicionado ponto e virgula na linha 15

--- dump.sql
+++ dump.sql
@@ -12,5 +12,5 @@ create table categorias (
     descricao varchar(1000) not null
 );
 
-INSERT INTO "categorias" (id,descricao) VALUES (1,'Informatica'),(2, 'Celulares'),(3, 'Beleza e perfumaria'),(4, 'Mercado'),
+INSERT INTO "categorias" (id,descricao) VALUES (1,'Informatica'),(2, 'Celulares'),(3, 'Beleza e perfumaria'),(4, 'Mercado');
 INSERT INTO "categorias" (id,descricao) VALUES (5,'Livros e Papelaria'),(6,'Brinquedos'),(7,'Moda'),(8,'Bebê'),(9,'Games');
