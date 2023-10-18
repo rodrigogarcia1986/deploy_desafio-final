a6f075d6a57f6de14b60a1bcfa6f9fdbb2862c6c
Patrick Rigaud <104288701+PatrickRigaud@users.noreply.github.com>
2023-10-18 12:06:48 -0300

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -12,7 +12,6 @@ create table categorias (
     descricao varchar(1000) not null
 );
 
-create table clientes (
 
 
 INSERT INTO "categorias" (id,descricao) VALUES (1,'Informatica'),(2, 'Celulares'),(3, 'Beleza e perfumaria'),(4, 'Mercado');
