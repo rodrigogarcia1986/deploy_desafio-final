855cdbbd5c12986f499277f0725f949ebab6b318
Larissa-Gnandt <larissagnandt@gmail.com>
2023-11-01 16:38:04 -0300

    Correção: importe produtoDados

--- src/controladores/pedidos.js
+++ src/controladores/pedidos.js
@@ -1,5 +1,6 @@
-const requisicao = require('../dados/pedidos-dados');
-const mensagem = require('../utilitarios/mensagens');
+const requisicao = require("../dados/pedidos-dados");
+const produtoDados = require("../dados/produto-dados");
+const mensagem = require("../utilitarios/mensagens");
 
 const listarPedidos = async (req, res) => {
   const { cliente_id } = req.query;
@@ -9,7 +10,9 @@ const listarPedidos = async (req, res) => {
       validarCliente = await requisicao.buscarCliente(cliente_id);
 
       if (!validarCliente) {
-        return res.status(400).json({ mensagem: mensagem.clienteNaoEncontrado });
+        return res
+          .status(400)
+          .json({ mensagem: mensagem.clienteNaoEncontrado });
       }
 
       const pedido = await requisicao.listarPedidoCliente(cliente_id);
@@ -21,11 +24,10 @@ const listarPedidos = async (req, res) => {
 
     return res.status(200).json(pedidos);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).json({ mensagem: mensagem.erroInterno });
   }
-
-}
+};
 
 const cadastrarPedido = async (req, res) => {
   const { observacao, pedido_produtos, cliente_id } = req.body;
